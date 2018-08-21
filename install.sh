00dfc453e49b40455c78122ced487bd2e4dc09fd
Jannik Ewers <V-Modder@web.de>
2018-08-22 01:01:46 +0200

    bin

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-desktopPath=$(echo ~)
+desktopPath=$(echo ~)/Desktop
 [ -e AlarmClock.jar ] && rm AlarmClock.jar
 [ -e alarm_clock.png ] && rm alarm_clock.png
 [ -e ${desktopPath}/AlarmClock.desktop ] && rm ${desktopPath}/AlarmClock.desktop
@@ -12,18 +12,18 @@ wget -O alarm_clock.png https://github.com/V-Modder/AlarmClock/blob/master/bin/a
 jarPath=$(realpath AlarmClock.jar)
 pngPath=$(realpath alarm_clock.png)
 
-echo << EOF
+desktopContent=$(cat <<EOF
 [Desktop Entry]
-Encoding=UTF-8
-Version=1.0
-Name[en_US]=AlarmClock
-GenericName=Alarm Clock
-Exec=java -jar ${jarPath}
-Terminal=false
-Icon[en_US]=${pngPath}
-Type=Application
-Categories=Application
-Comment[en_US]=Simple alarm clock
-EOF > ${desktopPath}/AlarmClock.desktop
+ Name=Alarm-Clock
+ Comment=Simple alarm clock
+ Icon=${pngPath}
+ Exec=java -jar ${jarPath}
+ Type=Application
+ Encoding=UTF-8
+ Terminal=false
+ Categories=Application
+EOF
+)
+echo "${desktopContent}" > ${desktopPath}/AlarmClock.desktop
 
 exec java -jar ${jarPath} &
