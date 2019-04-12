830ed0b0f991aace31dc5203f9e30ee0bd8040f1
Jannik Ewers <V-Modder@web.de>
2019-04-12 23:10:14 +0200

    update install

--- install.sh
+++ install.sh
@@ -1,31 +1,93 @@
 #!/bin/bash
 set -e
 
-desktopPath=$(echo ~)/Desktop
-[ -e AlarmClock.jar ] && rm AlarmClock.jar
-[ -e alarm_clock.png ] && rm alarm_clock.xpm
-[ -e ${desktopPath}/AlarmClock.desktop ] && rm ${desktopPath}/AlarmClock.desktop
 
-wget -O AlarmClock.jar  https://raw.githubusercontent.com/V-Modder/AlarmClock/master/bin/AlarmClock.jar
-wget -O alarm_clock.xpm https://raw.githubusercontent.com/V-Modder/AlarmClock/master/bin/alarm_clock.xpm
+function main() {
+  local install=$1
+  local desktopPath=$(echo ~)/Desktop
 
-jarPath=$(realpath AlarmClock.jar)
-pngPath=$(realpath alarm_clock.xpm)
-workingDir=$(realpath .)
 
-desktopContent=$(cat <<EOF
+  if [ $install -eq 0 ]; then
+    echo "Updating install script"
+    removeExistingFiles $desktopPath
+    downloadInstallScript
+    exec ./install.sh --install
+  else
+    echo "Updating application"
+    downloadFiles
+    local jarPath=$(realpath AlarmClock.jar)
+    local iconPath=$(realpath alarm_clock.xpm)
+    local workingDir=$(realpath .)
+    createDesktopShortcut $jarPath $workingDir $iconPath $desktopPath
+    execJar $jarPath
+  fi
+}
+
+function downloadInstallScript() {
+  echo "Downloading install script" 
+  curl -s https://api.github.com/repos/V-Modder/AlarmClock/releases/latest \
+    | grep "browser_download_url.*install.sh" \
+    | cut -d '"' -f 4 \
+    | wget -qi -
+}
+
+
+function downloadFiles() {
+  echo "Downloading files"
+  curl -s https://api.github.com/repos/V-Modder/AlarmClock/releases/latest \
+  | grep "browser_download_url" \
+  | grep -v "install.sh" \
+  | cut -d '"' -f 4 \
+  | wget -qi -
+}
+
+function removeExistingFiles() {
+  echo "Removing existing files"
+  local desktopPath=$1
+  removeFileIfExists "AlarmClock.jar"
+  removeFileIfExists "alarm_clock.xpm"
+  removeFileIfExists "${desktopPath}/AlarmClock.desktop"
+}
+
+function removeFileIfExists() {
+  if [ -e "${1}" ]; then
+    echo "Removing file ${1}"
+    rm ${1}
+  fi
+}
+
+function createDesktopShortcut() {
+  echo "Creating shortcut"
+  local jarPath=$1
+  local workingDir=$2
+  local iconPath=$3
+  local desktopPath=$4
+
+  local content=$(cat <<EOF
 [Desktop Entry]
- Name=Alarm-Clock
- Comment=Simple alarm clock
- Icon=${pngPath}
- Exec=java -jar ${jarPath}
- Path=${workingDir}
- Type=Application
- Encoding=UTF-8
- Terminal=false
- Categories=Application
+Name=Alarm-Clock
+Comment=Simple alarm clock
+Icon=${iconPath}
+Exec=java -jar ${jarPath}
+Path=${workingDir}
+Type=Application
+Encoding=UTF-8
+Terminal=false
+Categories=Application
 EOF
-)
-echo "${desktopContent}" > ${desktopPath}/AlarmClock.desktop
+  )
+  echo "${content}" > ${desktopPath}/AlarmClock.desktop
+}
 
-exec java -jar ${jarPath} &
+function execJar() {
+  echo "Starting Alarmclock"
+  local jarPath=$1
+  exec java -jar ${jarPath} &
+}
+
+install=0
+if [ "$1" = "--install" ]; then
+  install=1
+fi
+
+main $install
